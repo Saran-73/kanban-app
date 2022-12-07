cede8371df7d669fae1aae2cf17f87b81bbe3c5a
saran-73 <saransamikannu@gmail.com>
2022-12-07 17:36:17 +0530

    added menu for each card

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -8,7 +8,12 @@ import AppMenu from './CustomElements/AppMenu'
 import { BiDotsHorizontalRounded } from 'react-icons/bi';
 
 
-function TaskCard({ name, handleDragStart, singleBoardContents }: { name: string, handleDragStart: any , singleBoardContents: EachBoardType}) {
+function TaskCard({ name, handleDragStart, singleBoardContents }: { name: string, handleDragStart: any, singleBoardContents: EachBoardType }) {
+    const [cursorIn, setCursorIn] = React.useState(false)
+
+    const handleMouseEnter = () => setCursorIn(true)
+    const handleMouseLeave = () => setCursorIn(false)
+
     return (
         <AppCard
             variant="taskCard"
@@ -16,17 +21,24 @@ function TaskCard({ name, handleDragStart, singleBoardContents }: { name: string
             onDragStart={() => {
                 handleDragStart(singleBoardContents)
             }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
         >
-            {/* <Divider /> */}
-            {/* <AppMenu menuItems={[{text:"Edit task"}, {text: "Delete task", color:"red"}]}>
-                <AppIcon iconName={BiDotsHorizontalRounded} />
-            </AppMenu> */}
-            
-            <Text>Work life balance</Text>
+
+            <Flex justifyContent="space-between">
+                <Text>Work life balance</Text>
+                {cursorIn && (
+                    <AppMenu menuItems={[{ text: "Edit task" }, { text: "Delete task", color: "red" }]}>
+                        <AppIcon iconName={BiDotsHorizontalRounded} />
+                    </AppMenu>
+                )}
+            </Flex>
+
             <Flex alignItems="center" gap="0.45em">
                 <AppAvatar name={name} />
                 <Text fontSize="xs" color="gray.500">19, Jan 2021</Text>
             </Flex>
+
         </AppCard>
     )
 }
